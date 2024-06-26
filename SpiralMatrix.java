accdb9d93600b9d6b857f264742a3ef58467e993
DishaSasi1009 <164169511+DishaSasi1009@users.noreply.github.com>
2024-06-26 11:46:20 +0530

    Update SpiralMatrix.java

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -15,7 +15,6 @@ public class SpiralMatrixTraversal {
     public static void printSpiralOrder(int[][] matrix, int n) {
         int top = 0, bottom = n - 1, left = 0, right = n - 1;
         int val=1;
-        StringBuilder result = new StringBuilder();
 
         while (top <= bottom && left <= right) {
 
